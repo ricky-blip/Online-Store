415c24c532b5236e7ee944515123c74afecd6a54
ricky <ricky@pop-os.localdomain>
2022-10-16 10:47:24 +0700

    create splashscreen pushreplacement

--- ricky_store/lib/app/modules/splash_screen/views/splash_screen_view.dart
+++ ricky_store/lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -1,14 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:ricky_store/app/constant/color.dart';
+import 'package:ricky_store/app/routes/app_pages.dart';
 
 import '../controllers/splash_screen_controller.dart';
 
 class SplashScreenView extends GetView<SplashScreenController> {
   const SplashScreenView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    Timer(
+      Duration(seconds: 3),
+      () {
+        Get.offNamed(Routes.HOME);
+      },
+    );
+    // Get.offNamed(Routes.HOME);
     return const Scaffold(
       backgroundColor: appSoftBlue,
       body: Center(

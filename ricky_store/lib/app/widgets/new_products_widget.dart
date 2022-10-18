4fdbe59e281cae830073163863db5a7b354df6f2
ricky-blip <ricky.rinaldy77@gmail.com>
2022-10-18 19:29:07 +0700

    update UI product new

--- ricky_store/lib/app/widgets/new_products_widget.dart
+++ ricky_store/lib/app/widgets/new_products_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ricky_store/app/config/config.dart';
 import 'package:ricky_store/app/constant/color.dart';
 import 'package:ricky_store/app/data/models/product_new_model.dart';
@@ -12,33 +13,27 @@ class NewProductsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        InkWell(
-          onTap: () {},
-          child: Container(
-            width: 185,
-            child: Card(
-              shadowColor: appBlue,
-              color: appScaffoldBlue,
-              elevation: 5,
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Image.network(
-                      newProduct.gambar,
-                      width: MediaQuery.of(context).size.width,
-                      height: 170,
-                      fit: BoxFit.contain,
-                    ),
-                    SizedBox(height: 8),
-                    Text(newProduct.namaProduct),
-                    SizedBox(height: 7),
-                    Text(newProduct.harga.toString()),
-                    SizedBox(height: 29),
-                    Text("Rating"),
-                  ],
+        Card(
+          elevation: 3,
+          shadowColor: appBlue,
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              children: [
+                Ink.image(
+                  image: NetworkImage(newProduct.gambar),
+                  height: 250,
+                  width: 250,
+                  fit: BoxFit.contain,
                 ),
-              ),
+                Text(newProduct.namaProduct),
+                Text(newProduct.harga.toString()),
+                Text("Rating"),
+              ],
             ),
           ),
         ),
